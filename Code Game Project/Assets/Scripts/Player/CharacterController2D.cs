335e03b03dee4c84d4383cd42b187625f3e9e117
Magic-Stones <davesolayao123@gmail.com>
2023-08-17 06:36:17 +0800

    update 08/17/23
    - new input system
    - touch controls

---
+++ Code Game Project/Assets/Scripts/Player/CharacterController2D.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterController2D : MonoBehaviour
+{
+    public float ExternalMoveSpeed { get; set; } = 10f;
+    [SerializeField] private Vector2 _moveDirection;
+    private Rigidbody2D _rigidBody2D;   // Set Interpolate to "Interpolate"
+
+    [SerializeField] private AnimationClip _idleBottom, _idleTop, _idleRight, _idleLeft;
+    [SerializeField] private AnimationClip _walkBottom, _walkTop, _walkRight, _walkLeft;
+    private int _directionIndex = 1;
+
+    private PlayerInputActions _inputActions;
+    private InputAction _inputMove;
+
+    [SerializeField] private Animator _animatorSprite;
+
+    void OnEnable()
+    {
+        _inputMove = _inputActions.Player.Move;
+        _inputMove.Enable();
+    }
+
+    void OnDisable()
+    {
+        _inputMove.Disable();
+    }
+
+    void Awake()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+
+        _inputActions = new PlayerInputActions();
+    }
+
+    // Start is called before the first frame updates
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        CharacterAnimation();
+    }
+
+    void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void ProcessInput()
+    {
+        _moveDirection = _inputMove.ReadValue<Vector2>();
+
+        if (_moveDirection.y == 1 || _moveDirection.x >= 0.01 && _moveDirection.y >= 0.01)
+        {
+            _directionIndex = 2;
+        }
+        if (_moveDirection.x == -1 || _moveDirection.x <= -0.01 && _moveDirection.y >= 0.01)
+        {
+            _directionIndex = 4;
+        }
+        if (_moveDirection.y == -1 || _moveDirection.x <= -0.01 && _moveDirection.y <= -0.01)
+        {
+            _directionIndex = 1;
+        }
+        if (_moveDirection.x == 1 || _moveDirection.x >= 0.01 && _moveDirection.y <= -0.01)
+        {
+            _directionIndex = 3;
+        }
+    }
+
+    private void MoveCharacter()
+    {
+        _rigidBody2D.velocity = _moveDirection * ExternalMoveSpeed;
+    }
+
+    private void CharacterAnimation() 
+    {
+        switch (_directionIndex)
+        {
+            case 1:
+                if (_moveDirection != Vector2.zero) 
+                {
+                    _animatorSprite.Play(_walkBottom.name);
+                }
+                else
+                {
+                    _animatorSprite.Play(_idleBottom.name);
+                }
+                break;
+            
+            case 2:
+                if (_moveDirection != Vector2.zero) 
+                {
+                    _animatorSprite.Play(_walkTop.name);
+                }
+                else
+                {
+                    _animatorSprite.Play(_idleTop.name);
+                }
+                break;
+
+            case 3:
+                if (_moveDirection != Vector2.zero) 
+                {
+                    _animatorSprite.Play(_walkRight.name);
+                }
+                else
+                {
+                    _animatorSprite.Play(_idleRight.name);
+                }
+                break;
+
+            case 4:
+                if (_moveDirection != Vector2.zero) 
+                {
+                    _animatorSprite.Play(_walkLeft.name);
+                }
+                else
+                {
+                    _animatorSprite.Play(_idleLeft.name);
+                }
+                break;
+        }
+    }
+}
---
+++ Code Game Project/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+
+    private CharacterController2D _controller2D;
+
+    void Awake()
+    {
+        _controller2D = GetComponent<CharacterController2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller2D.ExternalMoveSpeed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
